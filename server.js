87dd6741d1e91cac22a9905e8c5cea96a0a18a98
agxcd <agxcdcode@gmail.com>
2021-01-31 23:38:18 -0800

    set up ejs environment

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const bodyParser = require("body-parser");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}! http://localhost:8000`);
+});
